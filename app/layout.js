f1864b6cb54aa496e494905241fe8eebc626ac07
rupsha014 <rupshasarkar14@gmail.com>
2023-06-03 20:43:59 -0700

    commit1

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['greek'] })
 
 export const metadata = {
   title: 'Create Next App',
