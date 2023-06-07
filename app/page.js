513011890d54108fef539448109e13e25e99a96f
rupsha014 <rupshasarkar14@gmail.com>
2023-06-06 23:44:15 -0700

    changes

--- app/page.js
+++ app/page.js
@@ -1,24 +1,31 @@
 import React from 'react'
-// import * as LottiePlayer from "@lottiefiles/lottie-player";
-import Lottie from 'react-lottie-player'
-import animations from '../public/assets/animations/instagram.json';
+
+// import Lottie from 'react-lottie-player'
+// import animations from '../public/assets/animations/instagram.json';
 
 function Home() {
   return (
     <div className=' font-bold text-4xl text-red-700 w-screen h-screen flex justify-center items-center'>
       <div className='flex  w-4/5 h-4/5 '>
         <div className='w-full h-full bg-gray-500'>
-        
-        <Lottie
+
+          {/* <Lottie
           loop
           animationData={animations}
           autoplay
           style={{ width: 150, height: 150 }}
           className='w-full h-full'
-        />
+        /> */}
         </div>
 
-        <div className='w-full h-full bg-black'>InstaClone2</div>
+        <div className='w-full h-full bg-black'>
+
+          <input type='email' name='email' id='email'></input>
+          <input type='password' name='password' id='password'></input>
+          <input></input>
+
+
+        </div>
       </div>
 
 
