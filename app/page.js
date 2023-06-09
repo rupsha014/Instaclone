46071aae88ec8146cdec342ae02bb2c8dcc36889
rupsha014 <rupshasarkar14@gmail.com>
2023-06-09 03:33:44 -0700

    adding

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 'use client'
-import React from 'react'
+import React, { useMemo } from 'react'
 
- import Lottie from 'react-lottie-player'
- import Animations from '@/public/assets/animations/instagram.json';
+import Lottie from 'react-lottie-player'
+import Animations from '@/public/assets/animations/instagram.json';
 import Button from '@/src/components/button'
 import useform from '@/src/hooks/useform'
 
@@ -18,44 +18,53 @@ const Home = () => {
   const submithandler = async (e) => {
     e.preventdefault();
     console.log(form);
+     
+
+    const isDisabled = useMemo(() => {
+
+          const isFormCorrect=Object.values(form).every((val)!=val)
+          console.log(isFormCorrect);
+    }
+    ,[form])
 
   }
   return (
-    <div className=' font-bold text-4xl text-white w-screen h-screen flex justify-center items-center'>
+    <div className=' font-bold text-2xl text-black w-screen h-screen flex justify-center items-center'>
       <div className='flex  w-4/5 h-4/5 '>
         <div className='w-full h-full bg-gray-500'>
 
           <Lottie
-          loop
-          animationData={Animations}
-          autoplay
-          style={{ width: 150, height: 150 }}
-          className='w-full h-full'
-        /> 
+            loop
+            animationData={Animations}
+            autoplay
+            style={{ width: 150, height: 150 }}
+            className='w-full h-full'
+          />
         </div>
 
-        <div className='w-full  bg-white border border-gray-300 '>
+        <div className='w-full  bg-white border border-gray-300  p-20'>
           <form onSubmit={submithandler} className='flex flex-col space-y-4'>
-    <div className='tracking-widest text-black'>Instagram</div>
+            <div className='tracking-widest text-black text-5xl my-5 justify-center flex'>Instagram</div>
 
             <input type='email'
               name='email'
               id='email'
               onChange={onChangeHandler}
               value={form?.email}
-              className='bg-gray-400 text-black '
-              placeholder='email' />
+              className='bg-gray-100 text-black p-2 outline  rounded-md hover:bg-transparent'
+              placeholder='Email' />
 
             <input type='password'
               name='password'
               id='password'
               onChange={onChangeHandler}
               value={form?.password}
-              className='bg-gray-400 text-black'
-              placeholder='password' />
+              className='bg-gray-100 text-black p-2 outline rounded-md hover:bg-transparent'
+              placeholder='Password' />
 
-            <button type='submit' className='bg-[#0095F6] py-2 px-6 text-white  active:scale-95 transform transition
-            '>
+            <button type='submit'
+             className='bg-[#0095F6] py-2 px-6 text-white  active:scale-95 transform transition rounded-md'
+              disabled={isDisabled}>
               Log in
             </button>
 
